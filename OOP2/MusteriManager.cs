bbd6c84ff6dc94135a4b33d1d9ce21be7140376d
ayselmehmetzade <ayselmehmetzade@hotmail.com>
2021-01-23 23:54:06 +0300

    Çalışma

---
+++ OOP2/MusteriManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            //if (musteri.)
+            //{
+
+            //}
+
+        }
+    }
+}
---
+++ OOP2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //engin demirog
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Engin";
+            musteri1.SoyAdi = "Demirog";
+            musteri1.TcNo = "12345678952";
+
+            //kodlama.io
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "1234565786";
+
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Ekle(musteri3);
+            musteriManager.Ekle(musteri4);
+           
+        }
+    }
+}
